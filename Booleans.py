f32d13a1f683efdde9cfadebb339da89bf93aac5
Panupong Mikada <panupong.mikada@gmail.com>
2023-04-03 07:36:22 +0000

    Booleans

---
+++ Booleans.py
@@ -0,0 +1,20 @@
+# Boolean Values
+
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+print("------------------------")
+
+# Evaluate Values and Variables
+
+print(bool("Hello"))
+print(bool(15))
+print(bool(["a", "b", "c"]))
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}))
+print("------------------------")
