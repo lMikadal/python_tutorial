855c3e462db5a89ae206b70e379172536adf53d6
Panupong Mikada <panupong.mikada@gmail.com>
2023-04-09 08:24:52 +0000

    Math

---
+++ Math.py
@@ -0,0 +1,37 @@
+# Built-in Math Functins
+
+x = min(3, 10, 25)
+y = max(3, 10, 25)
+
+print(x)
+print(y)
+print("---------------")
+
+t = abs(-5.555)
+
+print(t)
+print("---------------")
+
+p = pow(4, 3)
+
+print(p)
+print("---------------")
+
+# The Math Module
+
+import math
+
+x = math.sqrt(64)
+
+print(x)
+print("---------------")
+
+c = math.ceil(1.4)
+f = math.floor(1.4)
+
+print(c)
+print(f)
+print("---------------")
+
+print(math.pi)
+print("---------------")
